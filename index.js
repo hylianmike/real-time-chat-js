6a1247196082764c8c55a958ae07a70428bf7c86
Michael Rosanelli <michaelrosanelli105@gmail.com>
2023-04-06 14:47:02 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+var configs = require('./config');
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+var Message = mongoose.model("Message", {
+  name: String,
+  message: String,
+  date: Date,
+});
+
+app.get("/chat", (req, res) => {
+  Message.find({}, (error, messages) => {
+    res.send(messages);
+  }).sort({date: 1});
+});  
+
+app.post("/chat", (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) => {
+    if (err) sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  });
+});
+
+io.on('connection', () => { 
+    console.log('A User is connected!');
+});
+
+mongoose.connect(configs.db, (err) => {
+  console.log("MongoDB Connected", err);
+});
+
+var server = http.listen(3000, () => {
+  console.log("Server is running on port", server.address().port);
+});
---
+++ script.js
@@ -0,0 +1,31 @@
+var socket = io();
+
+$(() => {
+  $("#sendButton").click(() => {
+    sendMessage({
+      name: $("#name").val(),
+      message: $("#message").val(),
+      date: Date.now(),
+    });
+    document.getElementById("message").value = "";
+  });
+  getMessages();
+});
+
+socket.on("message", addMessages);
+
+function addMessages(message) {
+  $("#chat").prepend(
+    `<h4> ${message.name} </h4> <p> ${message.message} </p>`
+  );
+}
+
+function getMessages() {
+  $.get("http://localhost:3000/chat", (data) => {
+    data.forEach(addMessages);
+  });
+}
+
+function sendMessage(message) {
+  $.post("http://localhost:3000/chat", message);
+}
